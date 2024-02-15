f32c1c17593e93ee0189829f12b3577784530c92
Arushi <arushi22.chd@gmail.com>
2024-02-15 12:56:15 +0530

    add redux toolkit

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -39,7 +39,7 @@ export const signin = async (req, res, next) => {
         }
         const validPassword = bcryptjs.compareSync(password, validUser.password);
         if(!validPassword){
-            return next(errorHandler(400,'Incorrect password'));
+            return next(errorHandler(400,'Invalid password'));
         }
         const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
         const {password: pass, ...rest} = validUser._doc;
