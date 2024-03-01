40ec469e091d50a0196b75d6b4fd1dc6cb725caa
Arushi <arushi22.chd@gmail.com>
2024-03-01 16:27:15 +0530

    Complete update user profile page functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -7,11 +7,11 @@ export const test = (req,res) => {
 }
 
 export const updateUser = async (req,res,next) => {
-    if(req.user.id != req.params.userId){
+    if(req.user.id !== req.params.userId){
         return next(errorHandler(403, 'You are not allowed to update this user'));
     }
     if(req.body.password){
-        if(req.body.password > 6){
+        if(req.body.password.length < 6){
             return next(errorHandler(400, 'Password must be at least 6 characters'));
         }
         req.body.password = bcryptjs.hashSync(req.body.password, 10); 
@@ -29,6 +29,7 @@ export const updateUser = async (req,res,next) => {
         if(!req.body.username.match(/^[a-zA-Z0-9]+$/)){
             return next(errorHandler(400, 'Username can contain letters and numbers'));
         }
+    }
         try {
             const updatedUser = await User.findByIdAndUpdate(req.params.userId,{
                 $set: {
@@ -43,5 +44,4 @@ export const updateUser = async (req,res,next) => {
         } catch (error) {
             next(error);
         }
-    }
-};
+    };
