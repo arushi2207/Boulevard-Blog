b49d6f53c49d057ff8416ec28dafb502b9204f82
Arushi <arushi22.chd@gmail.com>
2024-03-10 18:35:26 +0530

    Complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,34 @@
-// import React from 'react'
-
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About Boulevard Blog
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              Welcome to Boulevard Blog! Hi, My name is Arushi and this blog was created by me
+              as a personal project to share my thoughts and ideas with the
+              world. I am a passionate developer who loves to write about
+              technology, coding, and everything in between.
+            </p>
+
+            <p>
+              On this blog, you'll find weekly articles and tutorials on topics
+              such as web development, software engineering, and programming
+              languages. I am always learning and exploring new
+              technologies, so be sure to check back often for new content!
+            </p>
+
+            <p>
+              I encourage you to leave comments on my posts and engage with
+              other readers. You can like other people's comments too. We believe that a community of learners can help
+              each other grow and improve.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
