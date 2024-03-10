c329dfc4ae03b32b76948b957ff007edfd2a95ad
Arushi <arushi22.chd@gmail.com>
2024-03-10 22:49:02 +0530

    Fix dp issue

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -81,11 +81,7 @@ export default function Header() {
             arrowIcon={false}
             inline
             label={
-              <Avatar
-                alt='user'
-                img={currentUser.profilPicture}
-                rounded
-              />
+              <Avatar alt='user' img={currentUser.profilePicture} rounded/>
             }
           >
             <Dropdown.Header>
